b5df1d406b9814efab9179f454674c4ad0160b77
Talita Pagani <talita.cpb@gmail.com>
2018-09-05 23:50:04 -0300

    Finish slider feature for home page

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,45 +3,46 @@
 document.addEventListener("DOMContentLoaded", function(e) {
 
 	function initSlider() {
-		const sliderItems		= document.getElementsByClassName('.newsCarousel-item');
+		const sliderItems		= document.querySelectorAll('.newsCarousel-item');
 		
 		var		sliderLength 	= sliderItems.length,
 					even 					= sliderLength / 2,
 					odd 					= (sliderLength + 1) / 2;
 
-		
+					console.log("slider length: ", sliderLength, " even: ", even, " odd:", odd);
 
-		sliderItems.forEach(function() {
-			this.addEventListener("click", function() {
+		sliderItems[0].classList.add("active");
+		sliderItems[1].classList.add('next');
+
+		sliderItems.forEach(function(el, i, arr) {
+			el.addEventListener("click", function() {
 				const sliderContainer = document.querySelector('.newsCarousel');
-			  //let slide = document.querySelector('.newsCarousel-item.active').width();
-			  //console.log(('.active').position().left);
+
+				let position = (((55 * i) + (15 * (i - 1))) * -1);
 			  
-			  if (this.className.indexOf('next') > -1) {
-			    sliderContainer.style.left = '-100vw';
-			  } else if (this.className.indexOf('prev') > -1) {
-			    sliderContainer.style.left = '100vw';
-			  }
+			  sliderContainer.style.transform = 'translateX(' + position + 'vw)';
 			  
-			  this.classList.remove('prev next');
-			  this.parentNode.children.forEach(function() {
-			  	this.classList.remove('prev active next');
+			  el.classList.remove('prev', 'next');
+			  //debugger;
+			  sliderItems.forEach(function(el, i, arr) {
+			  	el.classList.remove('prev', 'active', 'next');
 			  });
 			  
-			  this.classList.add('active');
-			  this.previousSibling.classList.add('prev');
-			  this.nextSibling.classList.add('next');
+			  el.classList.add('active');
+			  //debugger;
+			  if (el.previousElementSibling) el.previousElementSibling.classList.add('prev');
+			  if (el.nextElementSibling) el.nextElementSibling.classList.add('next');
 			});
 		});
 
 
 		// Keyboard nav
-		('html body').addEventListener("keydown", function(e) {
-		  if (e.keyCode == 37) { // left
-		    document.querySelector('.active').previousSibling.dispatchEvent( new MouseEvent("click", {bubbles: true}) );
+		document.body.addEventListener("keydown", function(e) {
+		  if (e.keyCode == 37) {
+		    document.querySelector('.active').previousElementSibling.dispatchEvent( new MouseEvent("click", {bubbles: true}) );
 		  }
-		  else if (e.keyCode == 39) { // right
-		    document.querySelector('.active').nextSibling.dispatchEvent( new MouseEvent("click", {bubbles: true}) );
+		  else if (e.keyCode == 39) {
+		    document.querySelector('.active').nextElementSibling.dispatchEvent( new MouseEvent("click", {bubbles: true}) );
 		  }
 		});
 	}
