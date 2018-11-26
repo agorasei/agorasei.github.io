e199c8243d1924ed76b854be723b7f26c3a822be
Talita Pagani <talita.cpb@gmail.com>
2018-11-26 21:35:46 -0200

    Adjustment in carousel for home page and minor adjustments in detail

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", function(e) {
 	if (document.documentElement.offsetWidth <= 600) {
 		initSlider();
 		initNewsNavigation();
+
+	    var elems = document.querySelectorAll('.parallax');
+	    var instances = M.Parallax.init(elems, {});
 	}
 
 	initUpdateReadingScroll(-1);
--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,31 @@
 document.addEventListener("DOMContentLoaded", function(e) {
 
-	initSlider();
-	initNewsNavigation();
+	var elem 		= document.querySelector('.carousel'),
+		options 	= {
+			duration: 	200,
+			dist: 		-60,
+			padding: 	20,
+			shift: 		20,
+			numVisible: 1,
+			noWrap: 	true,
+			fullWidth: 	false,
+			indicators: true
+		},
+		instance 	= M.Carousel.init(elem, options);
+
+		document.addEventListener("keydown", function(e) {
+
+			switch(e.keyCode) {
+				case 37:
+				case 38:
+					instance.prev();
+					break;
+				case 39:
+				case 40:
+					instance.next();
+					break;
+			}
+
+		});
 
 });
